7369f724a7955d8ecae2be5fdb95131d6c5721a6
Alex Gonzalez <alex.gonzalez@microsoft.com>
2023-03-03 12:00:57 +1100

    az_app_variables corrected namespace

--- az_app_variables/src/lib.rs
+++ az_app_variables/src/lib.rs
@@ -1,5 +1,5 @@
-pub use app_variables_new_macro::{New, GetFromEnv};
-pub use app_variables_new_macro_derive::New;
+pub use az_app_variables_new_macro::{New, GetFromEnv};
+pub use az_app_variables_new_macro_derive::New;
 
-pub use app_variables_init_macro::Init;
-pub use app_variables_init_macro_derive::Init;
+pub use az_app_variables_init_macro::Init;
+pub use az_app_variables_init_macro_derive::Init;
