5fbaf9584bacebfbc4f032a47019bb7644bcd2d6
Alex Gonzalez <alex.gonzalez@microsoft.com>
2023-03-03 11:44:24 +1100

    az_app_variables remove uneeded pub

--- az_app_variables/src/app_variables_new_macro/src/lib.rs
+++ az_app_variables/src/app_variables_new_macro/src/lib.rs
@@ -5,7 +5,7 @@ pub trait New {
 }
 
 pub trait GetFromEnv {
-    pub fn get_from_env(name: &str) -> String {
+    fn get_from_env(name: &str) -> String {
         match env::var(name) {
             Ok(value) => return value,
             Err(_) => {
